a97190f7e0b927bc6dbc4ca84a8ce4ea22aa41e3
unknown <anayjain@leeloolxp.com>
2021-08-13 13:23:50 +0530

    added os and browser code at logout

--- auth.php
+++ auth.php
@@ -330,6 +330,56 @@ class auth_plugin_leeloolxp_tracking_sso extends auth_plugin_base {
         $infoleeloolxp = json_decode($output);
 
         if ($infoleeloolxp->status != 'false') {
+
+            $u_agent = $_SERVER['HTTP_USER_AGENT'];
+            $bname = 'Unknown';
+            $platform = 'Unknown'; 
+
+            //First get the platform?
+            if (preg_match('/linux/i', $u_agent)) {
+                $platform = 'linux';
+            }
+            elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
+                $platform = 'mac';
+            }
+            elseif (preg_match('/windows|win32/i', $u_agent)) {
+                $platform = 'windows';
+            }
+
+            // Next get the name of the useragent yes seperately and for good reason
+            if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent))
+            {
+                $bname = 'Internet Explorer';
+                $ub = "MSIE";
+            }
+            elseif(preg_match('/Firefox/i',$u_agent))
+            {
+                $bname = 'Mozilla Firefox';
+                $ub = "Firefox";
+            }
+            elseif(preg_match('/Chrome/i',$u_agent))
+            {
+                $bname = 'Google Chrome';
+                $ub = "Chrome";
+            }
+            elseif(preg_match('/Safari/i',$u_agent))
+            {
+                $bname = 'Apple Safari';
+                $ub = "Safari";
+            }
+            elseif(preg_match('/Opera/i',$u_agent))
+            {
+                $bname = 'Opera';
+                $ub = "Opera";
+            }
+            elseif(preg_match('/Netscape/i',$u_agent))
+            {
+                $bname = 'Netscape';
+                $ub = "Netscape";
+            }
+            $user->browser = $bname;   
+            $user->os = $platform;   
+
             $leeloolxpurl = $infoleeloolxp->data->install_url; 
 
             $url = $leeloolxpurl . '/admin/sync_moodle_course/update_user_data_at_logout';
